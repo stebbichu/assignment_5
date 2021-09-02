5eb4c92cbde771c7bf20b9104b01c7fa0570d188
stebbichu <stebbichu@gmail.com>
2021-09-02 14:43:14 +0000

    sequence coded and works

--- sequence.py
+++ sequence.py
@@ -7,7 +7,14 @@ first_num, sec_num, third_num = 1, 0, 0
 #let's design a for loop to run the sequence
 for i in range(n):
     #add the numbers to get the sum
-    
+    sum = first_num + sec_num + third_num
     #print the sum
-    
+    print(str(sum))
     #replace the old numbers
+    if i == 0 or i == 1:
+        sec_num = first_num
+        first_num = sum
+    else:
+        third_num = sec_num
+        sec_num = first_num
+        first_num = sum
