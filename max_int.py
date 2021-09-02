ab6e369ece906178999852cb4644eeb4936286ba
stebbichu <stebbichu@gmail.com>
2021-09-02 13:55:04 +0000

    inital test

--- max_int.py
+++ max_int.py
@@ -1,3 +1,12 @@
 num_int = int(input("Input a number: "))    # Do not change this line
 # Fill in the missing code
+# We will promt the user for numbers until he inputs a negative number
+    #user inputs another number, 
+    #if the number is higher it replaces the old number
+
+
+
+
+
+
 print("The maximum is", max_int)    # Do not change this line
