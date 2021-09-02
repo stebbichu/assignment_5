b09097986fc221faef7030483ea093748f1d84db
stebbichu <stebbichu@gmail.com>
2021-09-02 14:06:22 +0000

    program works

--- max_int.py
+++ max_int.py
@@ -1,12 +1,13 @@
 num_int = int(input("Input a number: "))    # Do not change this line
 # Fill in the missing code
+max_int = 0
 # We will promt the user for numbers until he inputs a negative number
-    #user inputs another number, 
+while num_int >= 0:
     #if the number is higher it replaces the old number
-
-
-
-
-
+    if num_int > max_int:
+        max_int = num_int
+    #user inputs another number,
+    num_int = int(input())
+    
 
 print("The maximum is", max_int)    # Do not change this line
