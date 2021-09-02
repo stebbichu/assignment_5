0f15b46f52893501a3a5a2b9e5a66afc887c91b3
stebbichu <stebbichu@gmail.com>
2021-09-02 14:29:45 +0000

    Initial stage of sequence

---
+++ sequence.py
@@ -0,0 +1,13 @@
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+# Design an algorithm that generates the 
+# first n numbers in the following sequence:; 1, 2, 3, 6, 11, 20, 37, 
+
+sum = 0                                     
+first_num, sec_num, third_num = 1, 0, 0
+#let's design a for loop to run the sequence
+for i in range(n):
+    #add the numbers to get the sum
+    
+    #print the sum
+    
+    #replace the old numbers
