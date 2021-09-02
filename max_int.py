f6dd569ed56b496827298c7bfe834c40697e301a
stebbichu <stebbichu@gmail.com>
2021-09-02 14:12:45 +0000

    minor input Corrections

--- max_int.py
+++ max_int.py
@@ -7,7 +7,7 @@ while num_int >= 0:
     if num_int > max_int:
         max_int = num_int
     #user inputs another number,
-    num_int = int(input())
+    num_int = int(input("Input a number: "))
     
 
 print("The maximum is", max_int)    # Do not change this line
